f93e70e6d46f0255cefc9db0cc71788ea107b4c0
Pasquale <pasquale.coppola6@gmail.com>
2022-10-18 11:18:17 -0400

    questions project

---
+++ app.js
@@ -0,0 +1,21 @@
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn){
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.
+//         parentElement
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+//Using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+//console.log(question);
+const btn = question.querySelector('.question-btn');
+//console.log(btn);
+btn.addEventListener('click', function() {
+    question.classList.toggle('show-text');
+})
+});
